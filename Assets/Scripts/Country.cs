8e14e404171d1842c9ded58a3680fc4238b14f9e
Diego Martin <diegomartinrecillas@outlook.com>
2018-12-12 22:49:01 +0100

    working mvp

---
+++ Assets/Scripts/Country.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Country : MonoBehaviour  {
+
+    string fromLanguage = "en";
+    public string toLanguage = "en";
+
+    TextMeshPro textToGo;
+
+    private void Start()
+    {
+        string text = "beer";
+        string lang = fromLanguage + '-' + toLanguage;
+
+        textToGo = GetComponent<TextMeshPro>();
+        Translate(text, lang, textToGo);
+
+    }
+
+    Coroutine Translate(string text, string lang, TextMeshPro textMP) {
+        return StartCoroutine(TranslateAPI.GetTranslation(text, lang, textMP));
+    }
+}
---
+++ Assets/Scripts/TranslateAPI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
+
+
+[Serializable]
+class YandexResponse {
+    public int code;
+    public string lang;
+    public string[] text;
+}
+
+public static class TranslateAPI {
+
+    static public IEnumerator GetTranslation(string text, string lang, TextMeshPro textMP) {
+        // var jsonString = "This is expected to be sent back as part of response body.";
+        // byte[] byteData = System.Text.Encoding.ASCII.GetBytes(jsonString.ToCharArray());
+
+        UnityWebRequest unityWebRequest = new UnityWebRequest ("https://translate.yandex.net/api/v1.5/tr.json/translate" +
+			"?key=" + WWW.EscapeURL("trnsl.1.1.20181212T172242Z.a4bf1dcd55b4bf6c.e73a26e97b9840343904454402d4288d108ac746") +
+			"&text=" + WWW.EscapeURL(text) +
+			"&lang=" + WWW.EscapeURL(lang)
+		, "GET");
+		DownloadHandlerBuffer downloadHandlerBuffer = new DownloadHandlerBuffer();
+
+		unityWebRequest.downloadHandler = downloadHandlerBuffer;
+		// unityWebRequest.uploadHandler = new UploadHandlerRaw(byteData);
+		unityWebRequest.SetRequestHeader ("Content-Type", "application/json");
+
+        yield return unityWebRequest.SendWebRequest();
+
+		if(unityWebRequest.isNetworkError || unityWebRequest.isHttpError) {
+            textMP.text = "ERROR";
+            Debug.Log(unityWebRequest.error);
+		}
+		else {
+			string response = unityWebRequest.downloadHandler.text;
+            string textRes = JsonUtility.FromJson<YandexResponse>(response).text[0];
+            textMP.text = textRes.ToLower();
+            Debug.Log(response);
+            Debug.Log(textRes);
+		}
+	}
+}
