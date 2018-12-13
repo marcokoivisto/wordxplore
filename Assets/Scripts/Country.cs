14e3388e9193bcc8aae0eec6350edd15acfd2ad7
Marco Koivisto <marcokoivisto@me.com>
2018-12-13 02:27:40 +0100

    EDIT - Text is now following the camera angle

--- Assets/Scripts/Country.cs
+++ Assets/Scripts/Country.cs
@@ -6,6 +6,7 @@ public class Country : MonoBehaviour  {
     string lang;
     readonly string fromLanguage = "en";
     public string toLanguage = "en";
+    public Camera cameraToLookAt;
 
     TextMeshPro textMP;
 
@@ -16,6 +17,13 @@ public class Country : MonoBehaviour  {
 
         TranslationManager.Instance.Register(this);
         Translate(TranslationManager.Instance.GetText());
+    }
+
+    void Update () {
+        Vector3 v = cameraToLookAt.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt( cameraToLookAt.transform.position - v ); 
+        transform.Rotate(0,180,0);
     }
 
     public Coroutine Translate(string text) {
